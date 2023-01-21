10fc7d6e87a79959caba1ac793ca90d0005929ad
Ash <anarchaworld@gmail.com>
2023-01-21 00:11:58 -0800

    added post and get to talk to backend

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,28 +1,60 @@
-import React, {useState} from 'react'
-import Form from './Form'
-import Table from './Table'
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Form from "./Form";
+import Table from "./Table";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
-  function removeOneCharacter(index){
+  function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
-      return i !== index
+      return i !== index;
     });
     setCharacters(updated);
   }
 
-  function updateList(person){
-    setCharacters([...characters, person]);
+  function updateList(person) {
+    makePostCall(person).then((result) => {
+      if (result && result.status === 200) {
+        setCharacters([...characters, person]);
+      }
+    });
   }
+
+  async function fetchAll() {
+    try {
+      const response = await axios.get("http://localhost:5000/users");
+      return response.data.users_list;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async function makePostCall(person) {
+    try {
+      const response = await axios.post("http://localhost:5000/users", person);
+      return response;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  useEffect(() => {
+    fetchAll().then((result) => {
+      if (result) {
+        setCharacters(result);
+      }
+    });
+  }, []);
 
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
       <Form handleSubmit={updateList} />
     </div>
-  )
+  );
 }
 
 export default MyApp;
